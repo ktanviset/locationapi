98fec23cf3e66caae5acd2a9af33abd4e269fb83
Krisana Tanviset <tkrisana@outlook.co.th>
2019-07-04 02:16:28 +0700

    add project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace locationapi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
